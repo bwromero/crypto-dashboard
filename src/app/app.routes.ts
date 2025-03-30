f73a4d3c501bcb2702d75725dc83364f5af24605
Bryan Romero <bryan.romerodev@gmail.com>
2025-03-30 23:14:59 +0200

    Added route for layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: DashboardComponent
+        component: LayoutComponent,
+        children: [
+            {
+                path: '',
+                component: DashboardComponent
+            }
+        ]
     }
 ];
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LayoutComponent } from '../layout/layout.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [LayoutComponent],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
