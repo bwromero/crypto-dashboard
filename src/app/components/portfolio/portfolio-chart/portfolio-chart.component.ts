88437af9a897f5a9e1f12db5e04f4b03bcdc32b1
Bryan Romero <bryan.romerodev@gmail.com>
2025-04-13 22:43:38 +0200

    portfolio-chart: init

---
+++ src/app/components/portfolio/portfolio-chart/portfolio-chart.component.ts
@@ -0,0 +1,143 @@
+import { Component, ViewChild } from '@angular/core';
+import { 
+  ApexChart,
+  ChartComponent, 
+  ApexAxisChartSeries
+} from 'ng-apexcharts';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  markers: ApexMarkers;
+  title: ApexTitleSubtitle;
+  fill: ApexFill;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  tooltip: ApexTooltip;
+  stroke: ApexStroke;
+  annotations: ApexAnnotations;
+  colors: any;
+  toolbar: any;
+};
+
+@Component({
+  selector: 'app-portfolio-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio-chart.component.html',
+  styles: ``
+})
+export class PortfolioChartComponent {
+  @ViewChild("chart", { static: false }) chart!: ChartComponent;
+  public chartOptions!: Partial<ChartOptions>;
+  public activeOptionButton = "all";
+  public updateOptionsData: any = {
+    "1m": {
+      xaxis: {
+        min: new Date("28 Jan 2013").getTime(),
+        max: new Date("27 Feb 2013").getTime()
+      }
+    },
+    "6m": {
+      xaxis: {
+        min: new Date("27 Sep 2012").getTime(),
+        max: new Date("27 Feb 2013").getTime()
+      }
+    },
+    "1y": {
+      xaxis: {
+        min: new Date("27 Feb 2012").getTime(),
+        max: new Date("27 Feb 2013").getTime()
+      }
+    },
+    "1yd": {
+      xaxis: {
+        min: new Date("01 Jan 2013").getTime(),
+        max: new Date("27 Feb 2013").getTime()
+      }
+    },
+    all: {
+      xaxis: {
+        min: undefined,
+        max: undefined
+      }
+    }
+  };
+
+  constructor() {
+    this.initChart();
+  }
+
+  initChart(): void {
+    this.chartOptions = {
+      series: [
+        //{
+        //  data: data
+        //}
+      ],
+      chart: {
+        type: "area",
+        height: 350
+      },
+      annotations: {
+        yaxis: [
+          {
+            y: 30,
+            borderColor: "#999",
+            label: {
+              text: "Support",
+              style: {
+                color: "#fff",
+                background: "#00E396"
+              }
+            }
+          }
+        ],
+        xaxis: [
+          {
+            x: new Date("14 Nov 2012").getTime(),
+            borderColor: "#999",
+            label: {
+              text: "Rally",
+              style: {
+                color: "#fff",
+                background: "#775DD0"
+              }
+            }
+          }
+        ]
+      },
+      dataLabels: {
+        enabled: false
+      },
+      markers: {
+        size: 0
+      },
+      xaxis: {
+        type: "datetime",
+        min: new Date("01 Mar 2012").getTime(),
+        tickAmount: 6
+      },
+      tooltip: {
+        x: {
+          format: "dd MMM yyyy"
+        }
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shadeIntensity: 1,
+          opacityFrom: 0.7,
+          opacityTo: 0.9,
+          stops: [0, 100]
+        }
+      }
+    };
+  }
+
+  public updateOptions(option: any): void {
+    this.activeOptionButton = option;
+    this.chart.updateOptions(this.updateOptionsData[option], false, true, true);
+  }
+}
