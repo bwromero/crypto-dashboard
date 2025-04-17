869087be946a2b91f2d5e43d0799a1026235bba7
Bryan Romero <bryan.romerodev@gmail.com>
2025-04-17 12:49:03 +0200

    General layout: hide icon in header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styles: ``
 })
