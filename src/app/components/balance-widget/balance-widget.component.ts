775517e5b1d31fd6f841e367105ea3c240704184
Bryan Romero <bryan.romerodev@gmail.com>
2025-04-23 09:20:55 +0200

    Balance-widget: init

---
+++ src/app/components/balance-widget/balance-widget.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-balance-widget',
+  standalone: true,
+  imports: [],
+  templateUrl: './balance-widget.component.html',
+  styles: ``
+})
+export class BalanceWidgetComponent {
+
+}
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { PortfolioComponent } from '../portfolio/portfolio.component';
 import { ActivityHistoryComponent } from '../activity-history/activity-history.component';
-
+import { BalanceWidgetComponent } from '../balance-widget/balance-widget.component';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [PortfolioComponent, ActivityHistoryComponent],
+  imports: [PortfolioComponent, ActivityHistoryComponent, BalanceWidgetComponent],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
