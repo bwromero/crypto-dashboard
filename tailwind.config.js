85b75a095a753b7e81906ec18dab62de564acfaf
Bryan Romero <bryan.romerodev@gmail.com>
2025-05-06 16:35:49 +0200

    Tailwind: custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,25 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark-primary': 'var(--color-dark-primary)',
-        'dark-secondary': 'var(--color-dark-secondary)',
-        'dark-tertiary': 'var(--color-dark-tertiary)',
-        'text-primary': 'var(--color-text-primary)',
-        'text-secondary': 'var(--color-text-secondary)',
-        'accent': 'var(--color-accent)',
-        'success': 'var(--color-success)',
-        'warning': 'var(--color-warning)',
-        'error': 'var(--color-error)',
-        'action': 'var(--color-action)',
+        'dark-primary': '#171B21',
+        'dark-secondary': '#1A1F25',
+        'dark-tertiary': '#12181F',
+        'text-primary': '#FFFFFF',
+        'text-secondary': '#64748B',
+        'border': '#808195',
+        'accent': '#FF3E46',
+        'success': '#00C087',
+        'warning': '#F0B90B',
+        'error': '#FF3E46',
+        'gray': '#92929D',
+        'gray-light': '#F0F4F7',
+        'action': '#0CAF60',
+        'primary': '#FFFFFF',
+        'secondary': '#C2C2C2',
+        'light-gray': 'rgb(128 129 149 / 0.3)'
+      },
+      fontFamily: {
+        'display': ['Satoshi', 'sans-serif']
       }
     },
   },
