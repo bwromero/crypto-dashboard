829088925e608bcb2a6369dddc3beda5b58b815a
Bryan Romero <bryan.romerodev@gmail.com>
2025-10-19 21:40:42 +0200

    feat: init dropdown for num of rows

--- src/app/components/price-table/price-table.component.ts
+++ src/app/components/price-table/price-table.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
-
+import { ButtonComponent, DropdownOption } from '../../components/button/button.component';
 @Component({
   selector: 'app-price-table',
   standalone: true,
-  imports: [SearchBarComponent],
+  imports: [SearchBarComponent, ButtonComponent],
   templateUrl: './price-table.component.html',
   styles: ``
 })
 export class PriceTableComponent {
 
+ 
 }
