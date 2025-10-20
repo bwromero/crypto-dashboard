568840a2c3019845a65bacced5eeced2820feb0e
Bryan Romero <bryan.romerodev@gmail.com>
2025-10-20 13:37:11 +0200

    Feat: added table with mock data

--- src/app/components/live-prices/live-prices.component.ts
+++ src/app/components/live-prices/live-prices.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { PriceTableComponent } from './price-table/price-table.component';
 
 @Component({
   selector: 'app-live-prices',
   standalone: true,
-  imports: [ToolbarComponent],
+  imports: [ToolbarComponent, PriceTableComponent],
   templateUrl: './live-prices.component.html',
   styles: ``
 })
