5de6c2615a568e6fd0e329b3638afff5ecff762b
Bryan Romero <bryan.romerodev@gmail.com>
2025-10-20 17:26:12 +0200

    Feat: adding selection of views

--- src/app/components/live-prices/live-prices.component.ts
+++ src/app/components/live-prices/live-prices.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { PriceTableComponent } from './price-table/price-table.component';
+import { ButtonConfig } from '../button/button.component';
+import { CommonModule } from '@angular/common';
+import { HeatMapComponent } from './heat-map/heat-map.component';
+import { BubbleViewComponent } from './bubble-view/bubble-view.component';
 
 @Component({
   selector: 'app-live-prices',
   standalone: true,
-  imports: [ToolbarComponent, PriceTableComponent],
+  imports: [ToolbarComponent, PriceTableComponent, HeatMapComponent, BubbleViewComponent, CommonModule],
   templateUrl: './live-prices.component.html',
   styles: ``,
 })
-export class LivePricesComponent {}
+export class LivePricesComponent {
+
+  readonly views: string[] = ['list', 'heatmap', 'bubbles'];
+
+  selectedView: string = 'list';
+
+  onViewSelected(view: any) {
+    this.selectedView = view.name;
+    console.log(this.selectedView);
+  }
+}
