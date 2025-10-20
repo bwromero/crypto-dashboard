c02805b37212a3431737f70491a330a56003be9b
Bryan Romero <bryan.romerodev@gmail.com>
2025-10-20 11:10:57 +0200

    Feat: New sub components for live prices component

---
+++ src/app/components/live-prices/price-table/price-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-price-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './price-table.component.html',
+  styles: ``
+})
+export class PriceTableComponent {
+
+}
