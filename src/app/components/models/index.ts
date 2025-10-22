0385bffce19c5c782867feea420ddd85c5af58a6
Bryan Romero <bryan.romerodev@gmail.com>
2025-10-22 16:45:43 +0200

    Refactor: index.ts file

---
+++ src/app/components/models/index.ts
@@ -0,0 +1,6 @@
+import * as BubbleLayout from './bubble-layout';
+import * as CryptoInterface from './crypto.interface';
+import * as TreemapLayout from './treemap-layout';
+
+
+export { BubbleLayout, CryptoInterface, TreemapLayout };
