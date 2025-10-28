5c9f3f1d40f8955ab8148240ac869c25a9a022e1
Bryan Romero <bryan.romerodev@gmail.com>
2025-10-28 23:37:26 +0100

    Fix: removed bubble route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,17 +6,13 @@ export const routes: Routes = [
     loadComponent: () => import('./layout/layout.component').then(m => m.LayoutComponent),
     children: [
       {
-        path: '',
+        path: 'live-prices',
         loadComponent: () => import('./components/live-prices/live-prices.component').then(m => m.LivePricesComponent)
       },
       {
         path: 'dashboard',
         loadComponent: () => import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent)
-      },
-      {
-        path: 'bubble-view',
-        loadComponent: () => import('./components/live-prices/bubble-view/bubble-view.component').then(m => m.BubbleViewComponent)
-      }  
+      }
     ]
   }
 ];
