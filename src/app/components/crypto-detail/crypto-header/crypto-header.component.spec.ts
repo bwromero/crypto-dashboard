2a5a8357f081d0fc4bee5af11670d02ec8ad4b38
Bryan Romero <bryan.romerodev@gmail.com>
2025-10-30 12:12:50 +0100

    Refactor: create crypto-header component

---
+++ src/app/components/crypto-detail/crypto-header/crypto-header.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CryptoHeaderComponent } from './crypto-header.component';
+
+describe('CryptoHeaderComponent', () => {
+  let component: CryptoHeaderComponent;
+  let fixture: ComponentFixture<CryptoHeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CryptoHeaderComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(CryptoHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/crypto-detail/crypto-header/crypto-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './crypto-header.component.html',
+  styles: ``
+})
+export class CryptoHeaderComponent {
+
+}
