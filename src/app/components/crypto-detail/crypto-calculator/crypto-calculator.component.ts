e60ac64c0e8f315f8096d8b983898b30eee94afa
Bryan Romero <bryan.romerodev@gmail.com>
2025-10-31 10:20:16 +0100

    Feat: created crypto calculator component

---
+++ src/app/components/crypto-detail/crypto-calculator/crypto-calculator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-calculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './crypto-calculator.component.html',
+  styles: ``
+})
+export class CryptoCalculatorComponent {
+
+}
