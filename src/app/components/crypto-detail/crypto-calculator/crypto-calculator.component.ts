bb8adc8a0cea6d1954ff1d1ae86121cce11d42f4
Bryan Romero <bryan.romerodev@gmail.com>
2025-11-11 23:13:16 +0100

    Fix: icon size, gap and others

--- src/app/components/crypto-detail/crypto-calculator/crypto-calculator.component.ts
+++ src/app/components/crypto-detail/crypto-calculator/crypto-calculator.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Calculator, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-crypto-calculator',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './crypto-calculator.component.html',
   styles: ``
 })
 export class CryptoCalculatorComponent {
+
+  readonly calculatorIcon = Calculator;
 
 }
