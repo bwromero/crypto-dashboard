8dd8bec1a60e75bf7235937b021c3e9cb14f2d7d
Bryan Romero <bryan.romerodev@gmail.com>
2025-11-12 14:35:39 +0100

    Feat: created sign up component

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { ButtonComponent, ToggleOption } from '../shared/components/button/button.component';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './sign-up.component.html',
   styles: ``
 })
 export class SignUpComponent {
+
+  toggleOptions: ToggleOption[] = [
+    { label: 'Login', value: 'login' },
+    { label: 'SignUp', value: 'signup' }
+  ]
+
+  selectedToggleValue: string = 'SignUp';
+
+  onToggleChanged($event: string) {
+    throw new Error('Method not implemented.');
+  }
 
 }
