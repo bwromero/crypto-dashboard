e81bbfe8c0e2b362085f59ce841eb1cffef64397
Bryan Romero <bryan.romerodev@gmail.com>
2025-11-27 07:53:29 +0100

    Feat: added modal component to root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ModalComponent } from './components/shared/components/modal/modal.component';
 
 @Component({
     selector: 'app-root',
-    imports: [CommonModule, RouterOutlet],
+    imports: [CommonModule, RouterOutlet, ModalComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
